f346910800b7e7e18fa6698a4a6bbe919bc63c03
Danilo Pianini <danilo.pianini@gmail.com>
2023-05-16 15:44:27 +0200

    fix snippet 1

--- listings/SystemConfig.kt
+++ listings/SystemConfig.kt
@@ -2,17 +2,15 @@ object HighCPU : Capability
 object LowLatencyComm : Capability
 object EmbeddedDevice : Capability
 
-val confgiuration = pulverizationSystem {
-    device("device-1") {
-        Behaviour and State deployableOn HighCPU
-        Communication deployableOn LowLatencyComm
-        Sensors deployableOn EmbeddedDevice
-    }
-    device("device-2") {
-        Behaviour deployableOn
-            setOf(HighCPU, EmbeddedDevice)
-        Communication deployableOn LowLatencyComm
-        Sensors and Actuators deployableOn
-            EmbeddedDevice
-    }
+pulverizedSystem {
+  device("control-center") {
+    Behaviour and State deployableOn HighCPU
+    Communication deployableOn LowLatencyComm
+    Sensors deployableOn EmbeddedDevice
+  }
+  device("iot-sensor") {
+    Behaviour deployableOn setOf(HighCPU, EmbeddedDevice)
+    Communication deployableOn LowLatencyComm
+    Sensors and Actuators deployableOn EmbeddedDevice
+  }
 }
